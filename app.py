7ffee7ec2d863b1af8f361eb7f183d26bdfa9744
Thomas Bond <tbond4126@gmail.com>
2026-04-25 14:19:46 -0400

    Added early base for login functionality

--- app.py
+++ app.py
@@ -17,7 +17,23 @@ def index():
 # Login Page
 @app.route("/login")
 def login():
-   return render_template("login.html")
+   if (request.method == "GET"):
+      return render_template("login.html")
+   if (request.method == "POST"):
+      # Pulls user input from the html page
+      username = request.form["username"]
+      user_password = request.form["password"]
+      
+      # Renders different Pages depending on user permissions
+      if ("admin"):
+         pass
+      elif ("instructor"):
+         pass
+      elif ("student"):
+         pass
+      # If User not present in database, sends them back to login page
+      else:
+         return render_template("login.html")
 
 if (__name__ == "__main__"):
    #current port number was choosen arbitrarily
